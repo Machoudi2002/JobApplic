78db90fb037fc925ebfbac8d53becbd30d361aad
Machoudi2002 <stehat982@gmail.com>
2023-12-10 16:58:06 +0100

    add: Admin Register/Login functionality

---
+++ Backend/src/Controllers/AdminControllers.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+import { AdminSchema } from "../Models/AdminModel";
+
+const Admin = mongoose.model("Admin", AdminSchema)
+
+export const RegisterAdmin = async (req, res) => {
+    try {
+        let { email, password } = req.body
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newAdmin = new Admin({email, password: hashedPassword})
+        const admin = await newAdmin.save()
+        res.json(admin)
+    } catch (err) {
+        console.error('Registration error:', err.message);
+    }
+}
+
+export const LoginAdmin = async (req, res) => {
+    try {
+        let { email, password } = req.body;
+        const admin = await Admin.findOne({ email });
+        const passwordMatch = await bcrypt.compare(password, admin.password);
+        if (!admin) {
+            return res.status(401).json({ error: 'Invalid username or password' });
+        }
+        if (!passwordMatch) {
+            return res.status(401).json({ error: 'Invalid username or password' });
+        }
+        const token = jwt.sign({ userId: admin._id }, 'secret_key');
+        res.json({ token });
+    } catch (err) {
+        console.error('Login error:', err.message);
+    }
+    
+
+
+}
--- Backend/src/Routes/Routes.js
+++ Backend/src/Routes/Routes.js
@@ -1,6 +1,13 @@
 import { addNewJob, CreateNewJobApp, getJobById, getJobs } from "../Controllers/JobsControllers"
+import { RegisterAdmin, LoginAdmin } from "../Controllers/AdminControllers"
 
 export const appRouter = (app) => {
+    app.route("/LoginAdmin")
+    .post(LoginAdmin)
+    
+    app.route("/RegisterAdmin")
+    .post(RegisterAdmin)
+
     app.route("/jobs")
     .get(getJobs)
     .post(addNewJob)
